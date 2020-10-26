856cf3c94ca8e4228e5059b79e594dd2208cc762
e-graine <72887022+e-graine@users.noreply.github.com>
2020-10-26 12:15:53 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-data = {
-    title: "Itinéraire d'un burger",
+edata = {
+    title: "Food truck anti-gaspi",
     image: "img/image.jpg",
     zones : {
         bananes : {
@@ -8,67 +8,75 @@ data = {
             width : "30%",
             height : "11%",
             border : "1px solid black",
-            wrongText : "humm.. non ça c'est une banane",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
         torchon : {
             top : "28%",
             left : "10%",
             width : "40%",
             height : "12%",
-            wrongText : "Non le torchon ça marche pas",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
         listeCourse : {
             top : "30%",
             left : "60%",
             width : "35%",
             height : "20%",
-            wrongText : "Non la liste de courses ça marche pas",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
         yaourt : {
             top : "52%",
             left : "10%",
             width : "28%",
             height : "16%",
-            wrongText : "Non le yaourt ça marche pas",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
         frigoDuDesert : {
             top : "75%",
             left : "15%",
             width : "40%",
             height : "19%",
-            wrongText : "Non le frigo du désert ça marche pas",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
         chocolat : {
             top : "74%",
             left : "60%",
             width : "32%",
             height : "20%",
-            wrongText : "Non le chocolat ça marche pas",
+            wrongText : "Ce n'est pas la bonne réponse, essaye encore.",
         },
     },
     questions:
     [
         {
-            questionImage : "Retrouve un objet qui te permet d’économiser",
+            questionImage : "Les restaurateurs perdent beaucoup d'argent en achetant des produits qui ne seront pas utilisés. Retrouve un objet qui permettra aux restaurateurs d'économiser.",
             answerImage : "listeCourse",
-            questionText :"Quelle quantité de plats non déballés est jetée par an pour une personne ?",
+            questionText :"Faire ses menus et sa liste de course permet d'acheter seulement ce dont ont a besoin. Quelle quantité de plats non déballés est jetée par an pour une personne ?",
             propositions : ["7Kg", "2Kg", "10Kg", "5Kg"],
             answerText : "7Kg",
             correction: "Une personne jette 7 kg de plat non déballé par an. Faire sa liste de course permet d’acheter seulement ce dont on a besoin."
         },
         {
-            questionImage : "Retrouve un élément qui permet de conserver le pain plus longtemps",
+            questionImage : "Vous jetez trop de pain à burger. Trouvez un élément qui vous permettra de conserver le pain plus logtemps.",
             answerImage : "torchon",
-            questionText :"Quelle quantité de pain est gaspillée à la maison pour une personne ?",
-            propositions : ["4Kg", "500g", "1Kg", "3Kg"],
+            questionText :"Bonne réponse. Conserver le pain dans un torchon permet de garder l'humidité. Quelle quantité de pain est gaspillée à la maison pour une personne ?",
+            propositions : ["1Kg", "500g", "4Kg", "3Kg"],
             answerText : "4Kg",
-            correction: "Une personne jette 4 kg de pain par an en moyenne. Le pain se conserve plus longtemps dans un torchon, on peut aussi en faire du pain perdu."
+            correction: "Une personne jette 4 kg de pain par an en moyenne. Le pain rassis peut être utilisé pour de nombreuses fabrications : pain perdu, chapelure, bière."
         },
         {
-            questionImage : "Retrouve un aliment qui se consomme encore 2 semaines après sa date de péremption.",
+            questionImage : "Une grande parties des fruits et légumes que l'on achete est jeté. Trouve un ingrédient avec lequel les restaurateur peuvent proposer un bon gateau anti-gaspi.",
+            answerImage : "bananes",
+            questionText :"Bonne réponse. On peut faire un gâteau avec les peaux de bananes. Quelle quantité de fruits et légumes est gaspillée par an pour une personne ?",
+            propositions : ["7 kg", "2 kg", "5 kg", "12 kg"],
+            answerText : "7 kg",
+            correction: "Une personne jete 7 kg de fruits et légumes par an. Il est possible de cuisiner une bonne partie de nos légumes abimés et de nos épluchures."
+        },
+        {
+            questionImage : "Certains aliments sont jetés alors qu'ils pourrait encore être consommés. Retrouve un aliment qui se consomme encore 2 semaines après sa date de péremption.",
             answerImage : "yaourt",
             questionText :"Le CO2 émis par la production d’un yaourt est équivalent à un trajet de :",
-            propositions : ["12 km en voiture", "20 km en voiture", "3 km en voiture", "9 km en voiture"],
+            propositions : ["20 km en voiture", "9 km en voiture", "3 km en voiture", "12 km en voiture"],
             answerText : "12 km en voiture",
             correction: "La production de nos aliments a nécessité des ressources et du transport. Lorsque l’on jette un produit, ce sont toutes ses ressources utilisées qui sont gaspillées."
         },
