5f22990fd47a380e513c98a76adec8bbe9b69f70
e-graine <72887022+e-graine@users.noreply.github.com>
2020-10-26 14:01:08 +0100

    Update data.js

--- data.js
+++ data.js
@@ -65,6 +65,14 @@ edata = {
             correction: "Une personne jette 4 kg de pain par an en moyenne. Le pain rassis peut être utilisé pour de nombreuses fabrications : pain perdu, chapelure, bière."
         },
         {
+            questionImage : "Une grande parties des fruits et légumes que l'on achete est jeté. Trouve un ingrédient avec lequel les restaurateur peuvent proposer un bon gateau anti-gaspi.",
+            answerImage : "bananes",
+            questionText :"Bonne réponse. On peut faire un gâteau avec les peaux de bananes. Quelle quantité de fruits et légumes est gaspillée par an pour une personne ?",
+            propositions : ["7 kg", "2 kg", "5 kg", "12 kg"],
+            answerText : "7 kg",
+            correction: "Une personne jete 7 kg de fruits et légumes par an. Il est possible de cuisiner une bonne partie de nos légumes abimés et de nos épluchures."
+        },
+        {
             questionImage : "Certains aliments sont jetés alors qu'ils pourrait encore être consommés. Retrouve un aliment qui se consomme encore 2 semaines après sa date de péremption.",
             answerImage : "yaourt",
             questionText :"Le CO2 émis par la production d’un yaourt est équivalent à un trajet de :",
